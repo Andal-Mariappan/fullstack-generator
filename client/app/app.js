232a832f886c5eae64a3b26fea84c88b1acb787d
tejas_mehta_ <tejascmehta@gmail.com>
2016-02-11 08:58:33 +0530

    Initial Commit

---
+++ client/app/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('mysqlAdminApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router'
+])
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
